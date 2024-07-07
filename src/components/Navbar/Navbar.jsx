b03b940ca6d91cd1758b11f9c95ae4e4785e0ed1
ankitjaat16x2x8 <ankitjaat3535@gmail.com>
2024-07-07 12:50:15 +0530

    authentication added commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,10 +4,12 @@ import logo from '../../assets/logo.png'
 import arrow_icon from '../../assets/arrow_icon.png'
 import { coincontext } from '../../context/coincontext'
 import { Link } from 'react-router-dom'
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Navbar = () => {
 
   const {setCurrency} = useContext(coincontext)
+  const { loginWithRedirect, isAuthenticated, logout } = useAuth0();
 
 const currencyHandler=(event)=>{
   switch (event.target.value){
@@ -24,7 +26,7 @@ const currencyHandler=(event)=>{
       break;
     
     }
-    default {
+    default : {
       setCurrency({name:"usd", symbol:"$"});
       break;
     }
@@ -49,7 +51,21 @@ const currencyHandler=(event)=>{
                 <option value="eur">EUR</option>
                 <option value="inr">INR</option>
             </select>
-            <button>Sign up <img src={arrow_icon} alt=""/></button>
+            {
+              isAuthenticated ?(
+                <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+      Logout
+    </button>
+                
+              ):(
+                <button onClick={() => loginWithRedirect()}>Login<img src={arrow_icon} alt=""/></button>
+                
+
+              )
+            }
+            
+            
+            
 
         </div>
 
--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,20 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import CoincontextProvider from './context/coincontext.jsx'
+import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain="dev-rimgeg8u3u40ewbs.us.auth0.com"
+    clientId="TSgGukYqSszmpBejtjJ786PY8qOA1I9k"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <BrowserRouter>
     <CoincontextProvider>
       <App />
     </CoincontextProvider>
     </BrowserRouter>
-  </React.StrictMode>,
+  </Auth0Provider>,
 )
